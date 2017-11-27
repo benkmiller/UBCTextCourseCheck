293a1dcc1ec565089e2b94058413b4ac6e761415
Ben Miller <benkenji@alumni.ubc.ca>
2017-11-26 20:18:43 -0800

    small adjustments, added environment variable for phone number

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # UBCTextCourseCheck
-Python3
-A simple program which texts you when your requested program becomes available at UBC.
+Python3\n
+A simple program which texts you when your requested program becomes available at UBC.\n
 Using Twilio to send text.
 
 
--- UBCnotifierTEXT.py
+++ UBCnotifierTEXT.py
@@ -1,34 +1,30 @@
 import os
 from twilio.rest import Client
-
-#try:
- #   print("url 1")
 from urllib.request import urlopen
-#except ImportError:
- #   print("get 3!")
 
 import re
 import time
 import smtplib
 import getpass
 
-#account_sid = "AC55dba9d66a35db90fba63b83bb0c087c"
-#auth_token = "8a942ef6d8196735491dc48c8b5ae787"
+#need twilio credientials to run
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+twilio_number = os.environ["TWILIO_NUMBER"]
 
+WAIT_TIME = 30
+
+#sends a message to 
 def sendMessage(message, phonenumber):
-	client = Client(account_sid, auth_token)
-
-	client.messages.create(
-	    to=phonenumber,
-	    from_='+12048176697',
-	    body=message
-	)
+    client = Client(account_sid, auth_token)
+    client.messages.create(
+        to=phonenumber,
+        from_=twilio_number,
+        body=message
+    )
 
 def check(url):
     response = urlopen(url).read()
-
     htmlText = response.decode("utf8")
 
     #total= re.search(totalSeats, htmlText)
@@ -44,7 +40,6 @@ def check(url):
         if general.group(1) != '0':
             return 1
     else:
-        #quit here
         print("Something went wrong, maybe you put the wrong url in or lost internet connection, try restarting")
         return 0
     if restricted:
@@ -56,28 +51,28 @@ def check(url):
         print("Something went wrong, maybe you put the wrong url in or lost internet connection, try restarting")
         return 0
 
-
 department = input("Enter department(all caps): ")
 course = input("Enter course number: ")
 section = input("Enter section number: ")
+
 #year = input("Enter year: ")
-phoneNumber = input("Enter phone number: ")
+phoneNumber = input("Enter phone number:(in format +xxxxxxxxxxx) ")
 restricted = input("Are restricted seats okay?(yes/no)")
 print("All set, you'll recieve a text when a seat opens up :)")
-#p = getpass.getpass()
 
-
+#scrape data off ubc sight, without violating terms >:)
 #totalSeats = re.compile("<td width=&#39;200px&#39;>Total Seats Remaining:</td><td align=&#39;left&#39;><strong>(.*?)</strong></td>")
 generalSeats = re.compile("<td width=&#39;200px&#39;>General Seats Remaining:</td><td align=&#39;left&#39;><strong>(.*?)</strong></td>")
 restrictedSeats = re.compile("<td width=&#39;200px&#39;>Restricted Seats Remaining\*:</td><td align=&#39;left&#39;><strong>(.*?)</strong></td>")
 
 url = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=" + department + "&course=" + course + "&section=" + section
 
+#check every once in a while to see if a seats available, notify you if there is!
 while True:
     status = check(url)
 
     if status == 1:
-        print("GENERAL SEAT AVAILABLE SENDING EMAIL")
+        print("GENERAL SEAT AVAILABLE SENDING MESSAGE")
         sendMessage('There is a general seat available in ' + department + ' ' + course + '! Grab it here: ' + url, phoneNumber)
         break
     if status == 2:
@@ -86,5 +81,5 @@ while True:
             sendMessage('There is a restricted seat available in ' + department + ' ' + course + '! Grab it here: ' + url, phoneNumber)
             break
     else:
-        time.sleep(30)
+        time.sleep(WAIT_TIME)
 
